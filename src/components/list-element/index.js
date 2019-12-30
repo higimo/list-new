71922dc29852fc26f7804773db9b2c527088dd4e
Dmitriy Utkin <higimo@artlebedev.ru>
2019-12-30 23:21:12 +0300

    Вывожу все данные в элементе списка

--- src/components/list-element/index.js
+++ src/components/list-element/index.js
@@ -2,7 +2,19 @@ import { Link } from 'preact-router'
 
 import './style.css'
 
-const ListElement = ({ id, title, child, parent, created_at, code, isAuthorized, ...other }) => {
+const ListElement = ({
+	id,
+	title,
+	child,
+	parent,
+	created_at,
+	code,
+	isAuthorized,
+	propertys,
+	values,
+	...other
+}) => {
+	const val = values.filter(i => i.item == id)
 	return (
 		<div className="element-node">
 			<div className="element-node__data">
@@ -14,6 +26,20 @@ const ListElement = ({ id, title, child, parent, created_at, code, isAuthorized,
 				<div className="element-node__title">
 					<Link href={`/list-list/${id}`}>{title}</Link>
 				</div>
+				{!!val.length && (
+					<div className="element-node__props">
+						{val.map(value => [
+							<div
+								className="element-node__props-name"
+								dangerouslySetInnerHTML={{__html: propertys[value.property].name}}
+							/>,
+							<div
+								className="element-node__props-value"
+								dangerouslySetInnerHTML={{__html: value.value}}
+							/>
+						])}
+					</div>
+				)}
 				<div className="element-node__meta">
 					{isAuthorized && (
 						<span className="element-node__link">
@@ -27,7 +53,13 @@ const ListElement = ({ id, title, child, parent, created_at, code, isAuthorized,
 				</div>
 			</div>
 			{!!child && child.map((item, iter) => (
-				<ListElement key={iter} {...item} isAuthorized={isAuthorized} />
+				<ListElement
+					key={iter}
+					{...item}
+					propertys={propertys}
+					values={values}
+					isAuthorized={isAuthorized}
+				/>
 			))}
 		</div>
 	)
